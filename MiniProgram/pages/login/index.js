86c6d1264f1dd9ce5e3fc5f009444d9d587d104a
闻不多 <11601904+not-much@user.noreply.gitee.com>
2024-01-03 16:35:30 +0800

    third

--- MiniProgram/pages/login/index.js
+++ MiniProgram/pages/login/index.js
@@ -7,139 +7,67 @@ let app = getApp();
 let that;
 Page({
 
-  /**S
-   * 页面的初始数据
-   */
-  data: {
-    role: ['学生', '教师'],
-    roleIndex: '',
-    who:'',
-  },
+       /**S
+        * 页面的初始数据
+        */
+       data: {
+              role: ['学生', '教师'],
+              roleIndex: 0
+       },
+       role(e) {
+              that.setData({
+                     roleIndex: e.detail.value
+              });
+       },
+       /**
+        * 生命周期函数--监听页面加载
+        */
+       onLoad: function (options) {
+              that = this;
+              that.setData({
+                     width: app.globalData.windowWidth,
+                     height: app.globalData.windowHeight
+              });
+       },
 
-  updatePerson: function(event) {
-    const value = event.detail.value;
-    let who = '';
-    if (value === '0') {
-      who = '学生';
-    } else if (value === '1') {
-      who = '教师';
-    }
+       /**
+        * 生命周期函数--监听页面显示
+        */
+       onShow: function () {
+              let loginStatus = wx.getStorageSync('s');
+              if (loginStatus) {
+                     wx.reLaunch({
+                            url: '../index/index',
+                     });
+                     return;
+              }
+       },
+       login(e) {
+              if (e.detail.value.userName == "") {
+                     m.showTost('请输入学号');
+                     return;
+              } else if (e.detail.value.userPwd == "") {
+                     m.showTost('请输入姓名');
+                     return;
+              } else {
+                     // m.showLoading('正在登录');
+                     if (e.detail.value.role == 0) {
+                            //测试代码
+                            // console.log(typeof(e.detail.value.userName))
+                            // console.log(typeof(e.detail.value.userPwd)) 
+                            m.student_signup(
+                                   e.detail.value.userName,
+                                    e.detail.value.userPwd
+                            )
+                     } else if (e.detail.value.role == 1) { //测试
+                            m.teacher_signup(
+                                   e.detail.value.userName,
+                                    e.detail.value.userPwd
+                            )
+                     }
+              }
+       },
+       get_account(){
 
-    that.setData({
-      roleIndex: event.detail.value
-    });
-
-    getApp().globalData.who = who;
-    // console.log(who)  //测试who输出结果
-  },
-
-  updateName:function(event){
-    
-      const usrname = event.detail.value
-      this.setData({
-        usrname: usrname
-      })
-      getApp().globalData.name = usrname
-    
-  },
-
-  updateID:function(event){
-    
-    const usrid = event.detail.value
-    this.setData({
-      usrid: usrid
-    })
-    getApp().globalData.id = usrid
-    
-  },
-
-  
-
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-    that = this;
-    that.setData({
-      width: app.globalData.windowWidth,
-      height: app.globalData.windowHeight
-    });
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-    let loginStatus=wx.getStorageSync('s');
-    if (loginStatus){
-      wx.reLaunch({
-        url: '../index/index',
-      });
-      return;
-    }
-  },
-  login(e) {
-    if (e.detail.value.userName == "") {
-      m.showTost('请输入学号');
-      return;
-    } else if (e.detail.value.userPwd == "") {
-      m.showTost('请输入密码');
-      return;
-    } else {
-      // m.showLoading('正在登录');
-      if (e.detail.value.role == 0) {
-//测试代码
-            wx.redirectTo({
-              url: '../kechengbiao/kechengbiao',
-            })
-          }
-        //
-        // m.sLogin({
-        //   code: e.detail.value.userName,
-        //   password: e.detail.value.userPwd
-        // }).then(res => {
-        //   console.log(res);
-        //   if (res.data.code == 200) {
-        //     wx.setStorageSync('s', res.data.data);
-        //     setTimeout(() => {
-        //       wx.reLaunch({
-        //         url: '../kechengbiao/kechengbiao',
-        //       });
-        //       wx.hideLoading();
-        //     }, 1500);
-        //   }else{
-        //     wx.hideLoading();
-        //     m.showModal('学号或密码错误');
-        //   };
-        // },res=>{
-        //   console.log(res);
-        // });
-      //} 
-      else if (e.detail.value.role == 1){
-//测试
-        wx.redirectTo({
-          url: '../qjshenhe/qjshenhe',
-        })
-        
-        // m.tLogin({
-        //   username: e.detail.value.userName,
-        //   password: e.detail.value.userPwd
-        // }).then(res=>{
-        //   if (res.data.code == 200) {
-        //     wx.setStorageSync('s', res.data.data);
-        //     setTimeout(() => {
-        //       wx.reLaunch({
-        //         url: '../index/index',
-        //       });
-        //       wx.hideLoading();
-        //     }, 1500);
-        //   } else {
-        //     wx.hideLoading();
-        //     m.showModal('账号或密码错误');
-        //   };
-        // });
-      }
-    }
-  }
+       }
 })
--- MiniProgram/pages/qiandao/qiandao.js
+++ MiniProgram/pages/qiandao/qiandao.js
@@ -1,66 +1,99 @@
 var app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
-  },
+       /**
+        * 页面的初始数据
+        */
+       data: {
+              stuID: '',
+              qiandaotime: '',
+              qiandaocode: ''
+       },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad:function(){
-    app.editTabBar();    //显示自定义的底部导航
-  },
+       // 更新学生ID
+       updatecode: function (e) {
+              this.setData({
+                     qiandaocode: e.detail.value
+              });
+              console.log(this.data.qiandaocode)
+       },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
+       // 签到函数
+       qiandao: function () {
+              // 获取当前时间
+              var now = new Date();
+              var year = now.getFullYear();
+              var month = now.getMonth() + 1;
+              var day = now.getDate();
+              var hour = now.getHours();
+              var minute = now.getMinutes();
+              var second = now.getSeconds();
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
+              // 添加前导零
+              month = month < 10 ? '0' + month : month;
+              day = day < 10 ? '0' + day : day;
+              hour = hour < 10 ? '0' + hour : hour;
+              minute = minute < 10 ? '0' + minute : minute;
+              second = second < 10 ? '0' + second : second;
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
+              // 格式化时间
+              var timeStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
+              // 设置签到时间
+              this.setData({
+                     qiandaotime: timeStr
+              });
+              console.log(this.data.qiandaotime)
+              var apiUrl = 'http://43.136.80.11:5000/student_manager/punch_in';
+              var that = this;
+              wx.request({
+                     url: apiUrl,
+                     method: 'POST',
+                     header: {
+                            'app': 'wx-app',
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                     },
+                     data: {
+                            'student_id': that.data.stuID,
+                            'punch_in_time': that.data.qiandaotime,
+                            'code': that.data.qiandaocode
+                     },
+                     success: function (res) {
+                            if (res.statusCode === 200) {
+                                   var result = res.data;
+                                   console.log(result.msg);
+                                   wx.showToast({
+                                          title: result.msg,
+                                          icon: 'success',
+                                          duration: 2000
+                                   });
+                            } else {
+                                   console.error('Error:', res.statusCode, res.data);
+                            }
+                     },
+                     fail: function (error) {
+                            console.error('Request failed:', error);
+                     }
+              });
+       },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
+       /**
+        * 生命周期函数--监听页面加载
+        */
+       onLoad: function () {
+              app.editTabBar(); //显示自定义的底部导航
+              var that = this;
+              // 获得缓存
+              wx.getStorage({
+                     key: 'stuId',
+                     success: function (res) {
+                            that.setData({
+                                   stuID: res.data
+                            });
+                     }
+              });
+       },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
+       // 其他生命周期函数...
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
-})
+});
